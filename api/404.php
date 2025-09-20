0f44030902c222d4441bbd82463846fdff0152c9
Tiago França <github@tiagofranca.com>
2025-09-20 20:00:19 -0300

    change 4040 page

--- api/404.php
+++ api/404.php
@@ -9,4 +9,27 @@ if (filter_var(request_any_get('tracy', false), FILTER_VALIDATE_BOOL)) {
     require_once __DIR__ . '/tracy.php';
 }
 
-app_abort(404, 'Not found');
+$uri = request_uri();
+
+if (!headers_sent()) {
+    header("HTTP/1.1 404 Not found");
+
+    http_response_code(404);
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Not Found "<?= $uri ?>"</title>
+</head>
+<body>
+    <h2>Not Found "<?= $uri ?>"</h2>
+
+    <h5>Go to <a href="/">home</a></h5>
+</body>
+</html>
