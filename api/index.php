62bc65c9c8eba82dd92fb098993c9678c02b8bd1
Tiago França <github@tiagofranca.com>
2025-09-20 19:25:16 -0300

    uncomment load bootstrap file

--- api/index.php
+++ api/index.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 ob_start();
 
-// require_once __DIR__ . '/../core/bootstrap.php';
+require_once __DIR__ . '/../core/bootstrap.php';
 
 if (filter_var(request_any_get('tracy', false), FILTER_VALIDATE_BOOL)) {
     require_once __DIR__ . '/tracy.php';
