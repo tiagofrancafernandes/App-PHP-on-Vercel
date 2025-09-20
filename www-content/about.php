f3f28293ecc1351cb800739f2f20d7cdb7165119
Tiago França <github@tiagofranca.com>
2025-09-20 20:07:18 -0300

    changed files

--- www-content/about.php
+++ www-content/about.php
@@ -5,6 +5,26 @@ $fileName = str_replace(
     '',
     $_SERVER['SCRIPT_FILENAME'] ?? '',
 );
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File: <?= $fileName ?></title>
+</head>
+<body>
+    <h3>
+        This is the "<?= $fileName ?>" file. Go to <a href="/">home</a>
+    </h3>
 
-echo sprintf('this is the %s file. Go to <a href="/">home</a>', $fileName);
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/page?page=about">Page ?</a></li>
+    </ul>
+    
+</body>
+</html>
